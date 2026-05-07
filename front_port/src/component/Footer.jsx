b2d94b8ac204f4d861fe3c0cc22244f0df5bfdb0
8446khan <khanshoaiburrhman@gmail.com>
2026-05-07 21:00:52 +0530

    Fixed admin routing

--- front_port/src/component/Footer.jsx
+++ front_port/src/component/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
 
@@ -12,7 +13,9 @@ const Footer = () => {
                 Built with React & Tailwind CSS.
 
             </p>
-            <a className="hover:text-orange-500" href="/admin">admin Dashboard</a>
+            <Link className="hover:text-orange-500" to="/admin">
+                Admin Dashboard
+            </Link>
 
         </footer>
 
